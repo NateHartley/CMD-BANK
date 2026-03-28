8fffd060e0c65f50456b9cf5a46636e019d1d376
NateHartley <nathanhartley82@gmail.com>
2026-03-28 22:32:20 +0000

    Add to default config data + clean up

--- src/cmdbank/paths.py
+++ src/cmdbank/paths.py
@@ -2,14 +2,15 @@ from platformdirs import user_data_dir, user_cache_dir, user_config_dir
 from pathlib import Path
 
 APP_NAME = "cmdbank"
-DATA_DIR = Path(user_data_dir(APP_NAME)) / "Data"   # macOS '/Users/<USERNAME>/Library/Application Support/cmdbank/Data'
-CACHE_DIR = Path(user_cache_dir(APP_NAME))          # macOS '/Users/<USERNAME>/Library/Caches/cmdbank'
-READCMD = CACHE_DIR / ".READCMD"                    # macOS '/Users/<USERNAME>/Library/Caches/cmdbank/.READCMD'
-CONFIG_DIR = Path(user_config_dir(APP_NAME))        # macOS '/Users/<USERNAME>/Library/Application Support/cmdbank'
-CONFIG = CONFIG_DIR / "config.toml"                 # macOS '/Users/<USERNAME>/Library/Application Support/cmdbank/config.toml'
+DATA_DIR = Path(user_data_dir(APP_NAME)) / "Data"
+CACHE_DIR = Path(user_cache_dir(APP_NAME))
+READCMD = CACHE_DIR / ".READCMD"
+CONFIG_DIR = Path(user_config_dir(APP_NAME))
+CONFIG = CONFIG_DIR / "config.toml"
 
-PLACEHOLDER_CONFIG_DATA = """
-(Placeholder config file for cmdbank)
+DEFAULT_CONFIG_DATA = """
+copy_command_to_clipboard = false
+sort_files_alphabetically = true
 """
 
 def init_paths():
@@ -20,4 +21,4 @@ def init_paths():
         READCMD.touch()
 
     if not CONFIG.exists():
-        CONFIG.write_text(PLACEHOLDER_CONFIG_DATA)
+        CONFIG.write_text(DEFAULT_CONFIG_DATA)
