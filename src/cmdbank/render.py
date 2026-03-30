396faf4d5dc7a48b6f89211c72d6319a3ce1b5de
NateHartley <nathanhartley82@gmail.com>
2026-03-30 14:06:05 +0100

    Slight restructure and var name tidy up. Also add function documentation

--- src/cmdbank/render.py
+++ src/cmdbank/render.py
@@ -3,34 +3,34 @@ from rich.panel import Panel
 from rich.table import Table
 from rich.columns import Columns
 
-def main_menu(s_c):
-    stored_commands = s_c
+### Renders main menu, including main menu outline box, "View Saved Commands" column, and "Menu Options" column
+def main_menu(stored_commands):
     console = Console()
 
-    # View Saved Commands column
-    table1 = Table(show_header=False, box=None, pad_edge=False)
-    table1.add_column("No.", style="green", width=2, justify="right")
-    table1.add_column("Option", style="cyan")
+    # "View Saved Commands" column
+    VSC_table = Table(show_header=False, box=None, pad_edge=False)
+    VSC_table.add_column("No.", style="green", width=2, justify="right")
+    VSC_table.add_column("Option", style="cyan")
 
-    if not stored_commands:
-        table1.add_row("", "[red](Directory is empty)[/red]")
-    else:
+    if stored_commands:
         for i, opt in enumerate(stored_commands, start=1):
-            table1.add_row(str(i)+")", opt)
+            VSC_table.add_row(str(i)+")", opt)
+    else:
+        VSC_table.add_row("", "[red](Directory is empty)[/red]")
 
-    # Menu Options column
-    table2 = Table(show_header=False, box=None, pad_edge=False)
-    table2.add_column("Option")
-    table2.add_row("[green]a)[/green] add")
-    table2.add_row("[green]e)[/green] edit")
-    table2.add_row("[green]d)[/green] delete")
-    table2.add_row("[green]q)[/green] quit")
+    # "Menu Options" column
+    MO_table = Table(show_header=False, box=None, pad_edge=False)
+    MO_table.add_column("Option")
+    MO_table.add_row("[green]a)[/green] add")
+    MO_table.add_row("[green]e)[/green] edit")
+    MO_table.add_row("[green]d)[/green] delete")
+    MO_table.add_row("[green]q)[/green] quit")
 
     # Panels inside of main box
     columns = Columns(
         [
-            Panel(table1, title="View Saved Commands", padding=(1,4)), # Don't set to white bc of light theme
-            Panel(table2, title="Menu Options", padding=(1,4)),
+            Panel(VSC_table, title="View Saved Commands", padding=(1,4)), # Don't set to white bc of light theme
+            Panel(MO_table, title="Menu Options", padding=(1,4)),
         ],
         equal=False,
         expand=True,
@@ -46,40 +46,38 @@ def main_menu(s_c):
         )
     )
 
-def command_file_content(text, cmd_path, f1, f2, cmd_num):
-    if cmd_path.stat().st_size == 0:
-        text.append("(File is empty)\n\n", style="red")
-    else:
+### Renders the content of a command file, including command numbering, commands, and comments
+def command_file_content(text, cmd_path, f1, f2):
+    cmd_num = 0
+    if cmd_path.stat().st_size != 0:
         previous_line = " "
-        for l in f2:
-
+        for line in f2:
             # If line is not blank, no else statement so ignoring all blank lines
-            if l.strip():
-
+            if line.strip():
                 # If reading the info text
-                if l[0] == "#":
-                    info = l[1:].strip()
+                if line[0] == "#":
+                    info = line[1:].strip()
                     text.append(info+"\n\n", style="italic")
-
                 # If reading the command text
                 else:
                     # If two commands next to each other, no space in between them
                     if previous_line[0] != "#" and previous_line != " ":
                         text.append("\n")
-
                     cmd_num += 1
                     number = "["+str(cmd_num)+"]"
                     f1.write("%d\n" % cmd_num)
-                    f1.write(l + "\n")
-                    command = l.rstrip()
+                    f1.write(line + "\n")
+                    command = line.rstrip()
                     text.append(number, style="yellow")
                     text.append(" ")
                     text.append(command+"\n", style="cyan")
-        
-                previous_line = l
+                previous_line = line
         return previous_line
+    else:
+        text.append("(File is empty)\n\n", style="red")
 
-def command_file_structure(console, text, cmd_path, previous_line, selected_cmd):
+### Renders the structure of the command file, including main file outline box, and back option
+def command_file_structure(console, text, cmd_path, previous_line, selected_cmd_file):
     # Back option
     if cmd_path.stat().st_size != 0:
         if previous_line[0] != "#":
@@ -94,7 +92,7 @@ def command_file_structure(console, text, cmd_path, previous_line, selected_cmd)
     console.print(
         Panel(
             text,
-            title="[bold]"+selected_cmd+"[/bold]",
+            title="[bold]"+selected_cmd_file+"[/bold]",
             border_style="green",
         )
     )
