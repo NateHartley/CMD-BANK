a93505f339dc8c89e97b81e68e9f526b82589c35
NateHartley <nathanhartley82@gmail.com>
2026-04-06 22:25:59 +0100

    Add settings option to main menu

--- src/cmdbank/main.py
+++ src/cmdbank/main.py
@@ -2,16 +2,16 @@
 import sys
 from .paths import init_paths
 from .render import main_menu as render_main_menu
-from .actions import add, edit, delete, view
+from .actions import add, edit, delete, view, settings
 from .errors import *
 from .utils import *
 
 ### Executes main menu actions (add, edit, delete, quit, view command file) based on user input, and invokes rendering
 def app():
-    return_to_menu = True
-    menu_options = ['a', 'e', 'd', 'q']
-    while return_to_menu:
-        stored_commands, return_to_menu = list_command_files()
+    return_to_main_menu = True
+    menu_options = ['a', 'e', 'd', 's', 'q']
+    while return_to_main_menu:
+        stored_commands, return_to_main_menu = list_command_files()
         render_main_menu(stored_commands)
         usr_input = get_user_input()
         print("")
@@ -19,15 +19,17 @@ def app():
             if usr_input in menu_options:
                 match usr_input:
                     case "a":
-                        add(stored_commands)
+                        add(stored_commands, menu_options)
                     case "e":
                         edit(stored_commands)
                     case "d":
                         delete(stored_commands)
+                    case "s":
+                        return_to_main_menu = settings()
                     case "q":
-                        return_to_menu = False
+                        return_to_main_menu = False
             else:
-                return_to_menu = view(stored_commands, usr_input)
+                return_to_main_menu = view(stored_commands, usr_input)
         except KeyboardInterrupt:
             error_keyboard_interrupt()
             sys.exit()
